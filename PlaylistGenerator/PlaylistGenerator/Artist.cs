6b1d714cae2344440e325577a261fd6302630c86
benjaminwlea <benjaminw.lea@gmail.com>
2025-08-14 12:01:29 +0200

    Adding solution-file and project files.

---
+++ PlaylistGenerator/PlaylistGenerator/Artist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistGenerator;
+
+public class Artist
+{
+    public string Name { get; init; }
+
+    public Artist(string name)
+    {
+        Name = name;
+    }
+}
---
+++ PlaylistGenerator/PlaylistGenerator/Track.cs
@@ -0,0 +1,16 @@
+﻿namespace PlaylistGenerator;
+
+public class Track
+{
+    public string Name { get; init; }
+    public Artist Artist { get; init; }
+
+    public Track(string name, Artist artist)
+    {
+       Name = name;
+       Artist = artist;
+    }
+
+    
+    
+}
